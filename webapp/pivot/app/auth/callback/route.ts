af9416bf9001b1ce03fcc2a3151e0a6e60d4ea0a
michaelsouliman <michaelsouliman@gmail.com>
2025-05-08 04:23:19 -0700

    add request type for vercel

--- webapp/pivot/app/auth/callback/route.ts
+++ webapp/pivot/app/auth/callback/route.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@/utils/supabase/server"
 import { NextResponse } from "next/server"
 
-export async function GET(request) {
+export async function GET(request: Request) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get("code")
 
@@ -13,4 +13,3 @@ export async function GET(request) {
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(new URL("/dashboard", request.url))
 }
-
